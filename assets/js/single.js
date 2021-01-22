f05f4366bacd61db09429705e073d56fc4a63a8c
davitshirkhanyan <davit.shirkhanyan@gmail.com>
2021-01-21 22:49:28 -0800

    add query parameters, web location and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 let issueContainerEl = document.querySelector("#issues-container");
 let limitWarningEl = document.querySelector("#limit-warning");
+let repoNameEl = document.querySelector("#repo-name");
 
 let getRepoIssues = function(repo) {
     let apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
@@ -16,7 +17,8 @@ let getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -69,7 +71,24 @@ let displayWarning = function(repo) {
 
     // append to warning container
     limitWarningEl.appendChild(linkEl);
-}
+};
 
-getRepoIssues("facebook/react");
-getRepoIssues("davitshirkhanyan/git-it-done");
+let getRepoName = function() {
+    let queryString = document.location.search;
+    let repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+    
+    
+
+};
+
+getRepoName();
+
+
+
